6268f692ddfd281ccbff2ea9eefde474b5926d7d
gabtongol <geybtongol@gmail.com>
2024-09-17 18:14:30 -0400

    removed BASE_DIR in settings

--- fittrackr/settings.py
+++ fittrackr/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fittrackr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'fittrackr',
+        'NAME': 'fittrackr',
     }
 }
 
