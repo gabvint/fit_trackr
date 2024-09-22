5b991eaaaa49d73f393523ec242baa0c5069263c
gabtongol <geybtongol@gmail.com>
2024-09-21 22:57:58 -0400

    form styling

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,6 +2,9 @@ from django import forms # type: ignore
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import NewUser, Workout, Meal
 from django.contrib.auth.forms import AuthenticationForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, HTML
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
@@ -44,6 +47,7 @@ class CustomUserChangeForm(UserChangeForm):
     fields = ('username', 'first_name', 'last_name', 'email', 'calorie_goal', 'workout_goal', 'meal_goal')
     
     
+    
 class WorkoutForm(forms.ModelForm):
   class Meta:
     model = Workout
@@ -52,7 +56,7 @@ class WorkoutForm(forms.ModelForm):
   def __init__(self, *args, **kwargs):
     available_days = kwargs.pop('available_days', None)
     workout_list = kwargs.pop('workout_list', [])
-    super().__init__(*args, **kwargs)
+    # super().__init__(*args, **kwargs)
 
 
     super().__init__(*args, **kwargs)
@@ -62,7 +66,40 @@ class WorkoutForm(forms.ModelForm):
       choices=[('', 'Select a workout')] + [(workout, workout) for workout in workout_list],
       required=False
       )
+  
+  
+    self.fields['muscle_group'].widget.attrs['placeholder'] = 'Enter muscle group'
+    self.fields['name'].widget.attrs['placeholder'] = 'Select a workout'
+    self.fields['sets'].widget.attrs['placeholder'] = 'Number of sets'
+    self.fields['reps'].widget.attrs['placeholder'] = 'Number of reps'
+    self.fields['calorie_lost'].widget.attrs['placeholder'] = 'Calories burned'
+    self.fields['notes'].widget.attrs['placeholder'] = 'Additional notes'
+
     
+    self.helper = FormHelper()
+    self.helper.form_class = 'w-96 md:w-[450px] mx-auto bg-offWhite p-8 rounded-lg shadow-md mt-12 mb-12 font-questrial'
+    self.helper.layout = Layout(
+        #style not working
+        Field('day'),
+        Field('muscle_group'),
+        Field('name'),
+        Field('sets'),
+        Field('reps'),
+        Field('calorie_lost'),
+        Field('notes'),
+        HTML(
+          """
+           <button type="submit" class="relative mt-4 rounded-full px-5 py-1 overflow-hidden group bg-black text-cream hover:bg-gradient-to-r
+                hover:from-black hover:to-grey hover:ring-2 hover:ring-offset-2 hover:ring-redorange transition-all ease-out duration-300">
+                    <span class='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white 
+                    opacity-10 rotate-12 group-hover:-translate-x-40 ease'></span>
+                    <span class='relative'>Submit</span>
+            </button>
+          """
+        ),
+    )
+    
+
   def clean_name(self):
         """Allow the 'name' field to accept dynamically added values, and make it optional for specific muscle groups."""
         name = self.cleaned_data.get('name')
@@ -89,5 +126,33 @@ class MealForm(forms.ModelForm):
     #         'placeholder': 'Search for a food...',
     #         'autocomplete': 'off'
     #     })
+    
+    
+    self.fields['name'].widget.attrs['placeholder'] = 'Enter workout name'
+    self.fields['calories'].widget.attrs['placeholder'] = 'Calories gained'
+    self.fields['notes'].widget.attrs['placeholder'] = 'Additional notes'
+
+        
+    self.helper = FormHelper()
+    self.helper.form_class = 'w-96 md:w-[450px] mx-auto bg-offWhite p-8 rounded-lg shadow-md mt-12 mb-12 font-questrial'
+    self.helper.layout = Layout(
+        #style not working
+        Field('name'),
+        Field('day'),
+        Field('meal'),
+        Field('calories'),
+        Field('notes'),
+        HTML(
+          """
+           <button type="submit" class="relative mt-4 rounded-full px-5 py-1 overflow-hidden group bg-black text-cream hover:bg-gradient-to-r
+                hover:from-black hover:to-grey hover:ring-2 hover:ring-offset-2 hover:ring-redorange transition-all ease-out duration-300">
+                    <span class='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white 
+                    opacity-10 rotate-12 group-hover:-translate-x-40 ease'></span>
+                    <span class='relative'>Submit</span>
+            </button>
+          """
+        ),
+    )
+    
 
   
